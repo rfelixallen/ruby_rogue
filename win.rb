18a65f93adfb1fcabb38ab4957f45887dc2a8c82
Robert Allen <rfelixallen@gmail.com>
2015-03-07 11:06:01 -0500

    cleaned up test window code

--- win.rb
+++ win.rb
@@ -8,10 +8,9 @@ init_screen
 test_window = Window.new(20, 20, 20, 20)
 test_window.box("*", "@")
 winder = test_window.subwin(20, 20, 20, 20)
+winder.setpos(2,3)
+winder.addstr("Subwindow!")
 
 winder.refresh
 winder.getch
 winder.close
-
-getch
-close_screen
