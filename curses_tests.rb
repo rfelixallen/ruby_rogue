e88eb0be21f7202101db7ade3eeca678cf0f1484
Robert Allen <rfelixallen@gmail.com>
2015-03-06 22:59:11 -0500

    figured out how to get characters in the corner. Im working on putting a permanent help line on the bottom of the screen

--- curses_tests.rb
+++ curses_tests.rb
@@ -1,2 +1,38 @@
 require 'curses'
 include Curses
+
+init_screen
+
+l = lines.to_i
+c = cols.to_i
+
+setpos(0,0)
+addch('1')
+refresh
+timeout=(500)
+
+setpos(0,(c - 1))
+addch('2')
+refresh
+timeout=(500)
+
+setpos((l - 1),0)
+addch('3')
+refresh
+timeout=(500)
+
+setpos((l - 1),(c - 1))
+addch('4')
+refresh
+
+setpos((lines - 5) / 2, (cols - 10) / 2)
+addstr("Lines: #{l}, Columns: #{c}")
+refresh
+
+getch
+
+flash
+beep
+
+endwin
+return 0
