90ca7b124b09a9c9a8c61f6a88400d1ecfb22b8a
Robert Allen <rfelixallen@gmail.com>
2015-03-07 16:12:03 -0500

    messin around with screen sizes

--- win.rb
+++ win.rb
@@ -2,19 +2,21 @@ require 'curses'
 include Curses
 
 # This is to test curses windows
+# Discovered that the size of the window is dependant on the terminal, and might fail if the terminal isnt the correct length.
 
 init_screen
 
-test_window = Window.new(20, 20, 20, 20)
+test_window = Window.new(20, 20, (lines - 10) / 2, (cols - 10) / 2)
 test_window.box("*", "@")
-winder = test_window.subwin(20, 20, 20, 20)
+test_window.refresh
+getch
+winder = test_window.subwin(20, 20, (lines - 10) / 2, (cols - 10) / 2)
 winder.setpos(2,3)
 winder.addstr("Subwindow!")
-
 winder.refresh
 winder.getch
 winder.close
-
+clear
 
 =begin
 world = Window.new(100, 100, 100, 100)
