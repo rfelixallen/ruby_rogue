910ad348daf48583a3696db01c63cf6405b508c8
Robert Allen <rfelixallen@gmail.com>
2015-03-07 08:58:08 -0500

    trying to get the delay to work

--- curses_tests.rb
+++ curses_tests.rb
@@ -9,30 +9,27 @@ c = cols.to_i
 setpos(0,0)
 addch('1')
 refresh
-timeout=(500)
+timeout=5000
+flash
 
 setpos(0,(c - 1))
 addch('2')
 refresh
-timeout=(500)
+timeout=(5000)
 
 setpos((l - 1),0)
 addch('3')
 refresh
-timeout=(500)
+timeout=(5000)
 
 setpos((l - 1),(c - 1))
 addch('4')
 refresh
+timeout=(5000)
 
 setpos((lines - 5) / 2, (cols - 10) / 2)
 addstr("Lines: #{l}, Columns: #{c}")
 refresh
-
 getch
 
-flash
-beep
-
-endwin
-return 0
+close_screen
