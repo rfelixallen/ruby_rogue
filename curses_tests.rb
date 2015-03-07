6e9cad7a7fed924afd4c79ea1a5e0decf43485f4
Robert Allen <rfelixallen@gmail.com>
2015-03-07 10:03:54 -0500

    dropped the timeout code since it wasnt working

--- curses_tests.rb
+++ curses_tests.rb
@@ -13,22 +13,18 @@ flash
 setpos(0,0)
 addch('1')
 refresh
-timeout = 5000 # Set to 5 seconds so that I can confirm the delay.
 
 setpos(0,(cols - 1))
 addch('2')
 refresh
-timeout=(5000)
 
 setpos((lines - 1),0)
 addch('3')
 refresh
-timeout=(5000)
 
 setpos((lines - 1),(cols - 1))
 addch('4')
 refresh
-timeout=(5000)
 
 setpos((lines - 5) / 2, (cols - 10) / 2)
 addstr("Lines: #{lines}, Columns: #{cols}")
