8dab99238f29ed3bd67044c63d3c2abf56422db9
Robert Allen <rfelixallen@gmail.com>
2015-03-07 19:11:13 -0500

    removed message, added noecho. going to work on moving character

--- move.rb
+++ move.rb
@@ -1,20 +1,34 @@
 require 'curses'
 include Curses
 init_screen
-curs_set(0)	# Gets rid of blinking cursor.
+crmode # Tell curses to only accept 1 character input
+noecho # Inputted characters wont show on the screen
+curs_set(0)	# Gets rid of blinking cursor
 start_color
 init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK) 
 player = '@'
 
+# Initialize the main window
 win = Window.new(20,20,0,0)
 win.box("|", "-")
 win.refresh
 getch
 
+# Initialize the sub window
 viewp = win.subwin(20, 20, 0, 0)
-viewp.setpos(2, 3)
-viewp.addstr("#{player}")
 viewp.refresh
-viewp.getch
 
-win.close
+while input = getch
+    viewp.setpos(2, 3)  # Add player as a test
+    viewp.addstr("#{player}")
+    case input
+    when 'q'
+    	break
+    else
+    	flash
+    	viewp.refresh
+    end
+end
+
+getch
+win.close # Quit the program
