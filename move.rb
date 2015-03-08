b24115188ae5d15b1717785546c0148da76cb44a
Robert Allen <rfelixallen@gmail.com>
2015-03-07 20:52:15 -0500

    restrict movement to within borders

--- move.rb
+++ move.rb
@@ -27,24 +27,32 @@ viewp.refresh
 while input = getch
     case input
     when 'w'
-    	px -= 1
-    	viewp.setpos(px, py)
-    	viewp.addstr("#{p}")
+    	px -= 1 if px > 1
+	    	viewp.setpos(px + 1, py)
+    		viewp.addstr(" ")
+	    	viewp.setpos(px, py)
+	    	viewp.addstr("#{p}")
     	viewp.refresh
     when 's'
-    	px += 1
-    	viewp.setpos(px, py)
-    	viewp.addstr("#{p}")
+    	px += 1 if px < 18
+		    viewp.setpos(px - 1, py)
+	    	viewp.addstr(" ")
+	    	viewp.setpos(px, py)
+	    	viewp.addstr("#{p}")
     	viewp.refresh
     when 'd'
-		py += 1
-		viewp.setpos(px, py)
-		viewp.addstr("#{p}")
+    	py += 1 if py < 18
+		    viewp.setpos(px, py - 1)
+	    	viewp.addstr(" ")
+			viewp.setpos(px, py)
+			viewp.addstr("#{p}")
 		viewp.refresh
 	when 'a'
-		py -= 1
-		viewp.setpos(px, py)
-		viewp.addstr("#{p}")
+    	py -= 1 if py > 1
+		    viewp.setpos(px, py + 1)
+	    	viewp.addstr(" ")
+			viewp.setpos(px, py)
+			viewp.addstr("#{p}")
 		viewp.refresh
     when 'q'
     	break
