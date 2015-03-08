b3383653cc40a2195c5f1163b482e54db810f26b
Robert Allen <rfelixallen@gmail.com>
2015-03-07 20:25:08 -0500

    omg it moves

--- move.rb
+++ move.rb
@@ -1,12 +1,16 @@
 require 'curses'
 include Curses
+
+p = '@'
+px = 2
+py = 2
+
 init_screen
 crmode # Tell curses to only accept 1 character input
 noecho # Inputted characters wont show on the screen
 curs_set(0)	# Gets rid of blinking cursor
 start_color
 init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK) 
-player = '@'
 
 # Initialize the main window
 win = Window.new(20,20,0,0)
@@ -16,19 +20,40 @@ getch
 
 # Initialize the sub window
 viewp = win.subwin(20, 20, 0, 0)
+viewp.setpos(px, py)  # Add player as a test
+viewp.addstr("#{p}")
 viewp.refresh
 
 while input = getch
-    viewp.setpos(2, 3)  # Add player as a test
-    viewp.addstr("#{player}")
     case input
+    when 'w'
+    	px -= 1
+    	viewp.setpos(px, py)
+    	viewp.addstr("#{p}")
+    	viewp.refresh
+    when 's'
+    	px += 1
+    	viewp.setpos(px, py)
+    	viewp.addstr("#{p}")
+    	viewp.refresh
+    when 'd'
+		py += 1
+		viewp.setpos(px, py)
+		viewp.addstr("#{p}")
+		viewp.refresh
+	when 'a'
+		py -= 1
+		viewp.setpos(px, py)
+		viewp.addstr("#{p}")
+		viewp.refresh
     when 'q'
     	break
-    else
+    when 'w'
     	flash
+    else
     	viewp.refresh
     end
 end
 
-getch
-win.close # Quit the program
+
+#win.close # Quit the program
