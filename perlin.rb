816d921b6ec635d9f2310cf42f39630b16f54811
Robert Allen <rfelixallen@gmail.com>
2015-03-10 00:09:28 -0400

    trying to get the 2d noise to use multiple tiles instead of one type

---
+++ perlin.rb
@@ -0,0 +1,15 @@
+require 'perlin_noise'
+
+noise = Perlin::Noise.new 1, :interval => 200 # 1 is the dimension, interval is the noise generator
+
+0.step(300, 0.1).each do |x|
+	n = rand(0..100)
+	if n == 1..20
+		puts '#' * (noise[x] * 60).floor
+	elsif n == 21..60 
+		puts '~' * (noise[x] * 60).floor
+	else
+		puts '@' * (noise[x] * 60).floor
+	end
+end
+
