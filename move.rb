15e7e86c79236fbf0732f9f6edcbdb7edb9d7838
Robert Allen <rfelixallen@gmail.com>
2015-03-15 00:49:33 -0400

    added in some terrain for fun. it also helps put the viewport in perspective. the map does fill in the entire window, but the viewport cuts it

--- move.rb
+++ move.rb
@@ -20,20 +20,20 @@ def center(subwin,px,py,max_lines,max_cols)
 	# Calculate player x,y as center
 	# Set r,c to be the top left corner of the window
 	# Set move(r,c)
-	r = px - (max_lines / 2)	# get player x and subtract it from the half point
-	c = py - (max_cols / 2)		# get player y and subtract it from the half point
+	r = px - (max_lines / 2)	# get player x and subtract it from the half point of window
+	c = py - (max_cols / 2)		# get player y and subtract it from the half point of window
 	h = subwin.maxx				# get current subwindow max x
 	w = subwin.maxy				# get current subwindow max y
 
 	# if c is greater than max_cols
-	if c + max_cols >= w
-		delta = w - (c + max_cols)
+	if c + w >= max_cols
+		delta = max_cols - (c + w)
 		cc = c + delta
 	else
 		cc = c
 	end
-	if r + max_lines >= h
-		delta = h - (r + max_lines)
+	if r + h >= max_lines
+		delta = max_lines - (r + h)
 		rr = r + delta
 	else
 		rr = r
@@ -48,6 +48,29 @@ def center(subwin,px,py,max_lines,max_cols)
 	subwin.move(r, c)
 end
 
+def simple_generate(window)
+	# Fill in all spaces with random 1-3 tiles.
+	x = window.maxx
+	y = window.maxy
+	i = 1
+	j = 1
+	while j < y - 1
+		while i < x - 1
+			window.setpos(j,i)
+			window.addstr("~")
+			window.refresh
+			i += 1
+			if i >= x - 1
+				j += 1
+				i = 1
+			end
+			if j >= y -1
+				break
+			end
+		end
+	end
+end
+
 max_lines = 40 # I set this because lines/cols were janky to work with.
 max_cols = 40
 
@@ -59,8 +82,9 @@ start_color
 init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK) 
 
 # Initialize the main window
-win = Window.new(max_lines * 2,max_cols * 2,0,0) # Make the window lines/cols the same for subwindow to make the border look nice
+win = Window.new(80,80,0,0) # Make the window lines/cols the same for subwindow to make the border look nice
 win.box("|", "-")
+simple_generate(win)
 win.refresh
 getch
 
@@ -69,11 +93,12 @@ start_x = win.maxx / 2
 start_y = win.maxy / 2
 
 # Initialize the sub window
-viewp = win.subwin(max_lines,max_cols, 0, 0)
+viewp = win.subwin(win.maxx / 2, win.maxy / 2, 0, 0)
 p = Character.new(start_x,start_y)
 win.setpos(p.px, p.py)  # Add player as a test
 win.addstr("#{p.symb}")
 win.refresh
+viewp.refresh
 
 # I could not keyboard input to work, use wasd instead
 while input = getch
@@ -85,7 +110,8 @@ while input = getch
 	    	win.setpos(p.px, p.py)
 	    	win.addstr("#{p.symb}")
 	    	center(viewp,p.px,p.py,win.maxx,win.maxy)
-    	viewp.refresh
+    	#viewp.refresh
+    	win.refresh
     when 's'
     	p.px += 1 if p.px < ((max_lines * 2) - 2)
 		    win.setpos(p.px - 1, p.py)
@@ -93,7 +119,8 @@ while input = getch
 	    	win.setpos(p.px, p.py)
 	    	win.addstr("#{p.symb}")
 	    	center(viewp,p.px,p.py,win.maxx,win.maxy)
-    	viewp.refresh
+    	#viewp.refresh
+    	win.refresh
     when 'd'
     	p.py += 1 if p.py < ((max_cols * 2) - 2)
 		    win.setpos(p.px, p.py - 1)
@@ -101,7 +128,8 @@ while input = getch
 	    	win.setpos(p.px, p.py)
 	    	win.addstr("#{p.symb}")
 	    	center(viewp,p.px,p.py,win.maxx,win.maxy)
-		viewp.refresh
+		#viewp.refresh
+    	win.refresh
 	when 'a'
     	p.py -= 1 if p.py > 1
 		    win.setpos(p.px, p.py + 1)
@@ -109,7 +137,8 @@ while input = getch
 	    	win.setpos(p.px, p.py)
 	    	win.addstr("#{p.symb}")
 	    	center(viewp,p.px,p.py,win.maxx,win.maxy)
-		viewp.refresh
+		#viewp.refresh
+    	win.refresh
     when 'q'
     	break
     else
