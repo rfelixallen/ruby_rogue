84f95b701181dd2e115cfd88d3a0ebde592e73e4
Robert Allen <rfelixallen@gmail.com>
2015-03-21 23:46:21 -0400

    field updates with current terminal window size

--- resize.rb
+++ resize.rb
@@ -1,6 +1,18 @@
 require 'curses'
 include Curses
 
+def refreshx(field,score)
+	stdscr.refresh
+	field.refresh
+	score.refresh
+end
+
+def clearx(field,score)
+	stdscr.clear
+	field.clear
+	score.clear
+end
+
 parent_x = 0
 parent_y = 0
 score_size = 3
@@ -21,7 +33,7 @@ clear
 
 field = stdscr.subwin(parent_y - score_size, parent_x, 0, 0)
 score = stdscr.subwin(score_size, parent_x, parent_y - score_size, 0)
-field.box("|", "-")
+field.box("|", "-") #box doesnt resize well
 score.box("|", "-")
 refresh
 getch
@@ -31,7 +43,32 @@ field.setpos(1,1)
 field.addstr("Field") 
 score.setpos(1,1)
 score.addstr("Score")
-refresh
+field.setpos((lines - 5) / 2, (cols - 10) / 2)
+field.addstr("x = #{parent_x}, y = #{parent_y}")
+refreshx(field,score)
+getch
+
+while 1
+	new_y = stdscr.maxy
+	new_x = stdscr.maxx
+
+	if (new_y != parent_y || new_x != parent_x)
+		clearx(field,score)
+
+		parent_x = new_x
+		parent_y = new_y
+
+		field.resize(new_y - score_size, new_x)
+		score.resize(score_size, new_x)
+		score.move(new_y - score_size, 0)
+
+		field.setpos((lines - 5) / 2, (cols - 10) / 2)
+		field.addstr("x = #{parent_x}, y = #{parent_y}")
+
+		refreshx(field,score)
+	end
+	refreshx(field,score)
+end
 
 score.close # free up memory
 refresh
