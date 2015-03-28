9a1333cf425d6056340689586b0a09a0109db37b
Robert Allen <rfelixallen@gmail.com>
2015-03-28 10:41:10 -0400

    tried using mvwprintw but it didnt work

--- move2.rb
+++ move2.rb
@@ -1,8 +1,8 @@
 require 'curses'
 include Curses
 
-def mvwprintw(window, y, x, symb)
-	window.setpos(y,x)
+def mvwprintw(window, x, y, symb)
+	window.setpos(x,y)
 	window.addch("#{symb}")
 end
 
@@ -27,13 +27,28 @@ def borders(field)
 	field.refresh
 end
 
-parent_x = 0
-parent_y = 0
+def resize_border(window,parent_x,parent_y)
+	new_y = stdscr.maxy
+	new_x = stdscr.maxx
+
+	if (new_y != parent_y || new_x != parent_x)
+		window.clear
+
+		parent_x = new_x
+		parent_y = new_y
+
+		window.resize(new_y, new_x)
+		borders(window)
+		window.setpos(lines / 2, cols  / 2)
+		window.addstr("x = #{parent_x}, y = #{parent_y}")
+		window.refresh
+	end
+	window.refresh
+end
 
 init_screen
 noecho
 curs_set(0)
-
 stdscr # initialize stdscr? Might be active by default
 
 parent_y = stdscr.maxy # Gets y of terminal screen
@@ -42,36 +57,12 @@ field = stdscr.subwin(parent_y, parent_x, 0, 0)
 borders(field)
 field.setpos(lines / 2, cols  / 2)
 field.addstr("x = #{parent_x}, y = #{parent_y}")
+#mvwprintw(field,lines / 2, cols  / 2,"x = #{parent_x}, y = #{parent_y}")
 refresh
 getch
 
-# Label the subwindows
-=begin
-field.setpos(1,1)
-field.addstr("Field") 
-field.setpos(lines / 2, cols / 2)
-field.addstr("Subwinow and borders drawn!")
-field.refresh
-getch
-=end
-
 while 1
-	new_y = stdscr.maxy
-	new_x = stdscr.maxx
-
-	if (new_y != parent_y || new_x != parent_x)
-		field.clear
-
-		parent_x = new_x
-		parent_y = new_y
-
-		field.resize(new_y, new_x)
-		borders(field)
-		field.setpos(lines / 2, cols  / 2)
-		field.addstr("x = #{parent_x}, y = #{parent_y}")
-		field.refresh
-	end
-	field.refresh
+	resize_border(field,parent_x,parent_y)
 end
 
 field.close # free up memory
