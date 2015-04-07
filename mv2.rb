7f9f719b1cedf2c1d417a4d3fec1b588caa88a81
Robert Allen <rfelixallen@gmail.com>
2015-04-06 22:27:40 -0400

    okay move finally works. i think the problem lies with the size of the parent window and subwindow

--- mv2.rb
+++ mv2.rb
@@ -4,12 +4,21 @@ include Curses
 init_screen
 noecho
 curs_set(0)
-stdscr # initialize stdscr? Might be active by default
+stdscr
 
-field = Window.new(100,100,0,0)
-viewp = field.subwin(50, 50, 1, 1)
+field = Window.new(20,20,0,0)
+viewp = field.subwin(20,20,0,0)
 refresh
+
+field.setpos(0,0)
+field.addstr("Window: (#{field.begx},#{field.begy})")
+field.refresh
+getch
+clear
+refresh
+
 viewp.move(3,3)
+viewp.setpos(0,0)
 viewp.addstr("Viewport Position = (#{viewp.begx},#{viewp.begy})")
 viewp.refresh
 getch
