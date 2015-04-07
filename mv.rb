07f21c046757e3e3aa35231f340db3f0203e1bbd
Robert Allen <rfelixallen@gmail.com>
2015-04-06 21:33:04 -0400

    tested if subwindows will also move, and it seems they do

--- mv.rb
+++ mv.rb
@@ -6,10 +6,11 @@ noecho
 curs_set(0)
 stdscr
 
-window = Window.new(10,10,0,0)
+window = Window.new(20,20,0,0)
+subwindow = window.subwin(20, 20, 0, 0)
 refresh
 window.setpos(0,0)
-window.addstr("(#{window.begx},#{window.begy})")
+window.addstr("Window: (#{window.begx},#{window.begy})")
 window.refresh
 getch
 clear
@@ -17,6 +18,21 @@ refresh
 
 window.move(2,2)
 window.setpos(0,0)
-window.addstr("(#{window.begx},#{window.begy})")
+window.addstr("Window: (#{window.begx},#{window.begy})")
 window.refresh
+getch
+window.clear
+window.refresh
+
+subwindow.setpos(0,0)
+subwindow.addstr("Sub: (#{subwindow.begx},#{subwindow.begy})")
+subwindow.refresh
+getch
+clear
+refresh
+
+subwindow.move(2,2)
+subwindow.setpos(0,0)
+subwindow.addstr("Sub: (#{subwindow.begx},#{subwindow.begy})")
+subwindow.refresh
 getch
