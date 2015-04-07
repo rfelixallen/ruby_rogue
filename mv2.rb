330da0eca138f64a196c552f0e9cde6a594edfdc
Robert Allen <rfelixallen@gmail.com>
2015-04-07 11:14:54 -0400

    expanded test code to get user input for window size, and to test default and move windows for parent and sub

--- mv2.rb
+++ mv2.rb
@@ -1,15 +1,26 @@
 require 'curses'
 include Curses
 
+# I've had problems with window.move working in other programs.
+# I've isolated the issue to the initial size of the window.
+# I dont know how the original 
+
 init_screen
-noecho
-curs_set(0)
 stdscr
 
-field = Window.new(20,20,0,0)
-viewp = field.subwin(20,20,0,0)
+addstr("Enter the square dimensions for the window (i.e. 10, 20, etc)")
+refresh
+x = getstr.to_i
+noecho
+curs_set(0)
+clear
 refresh
 
+field = Window.new(x,x,0,0)
+viewp = field.subwin(x,x,0,0)
+refresh
+
+#control message
 field.setpos(0,0)
 field.addstr("Window: (#{field.begx},#{field.begy})")
 field.refresh
@@ -17,7 +28,23 @@ getch
 clear
 refresh
 
-viewp.move(3,3)
+#test move message
+field.move(1,1)
+field.setpos(0,0)
+field.addstr("Window: (#{field.begx},#{field.begy})")
+field.refresh
+getch
+clear
+refresh
+
+viewp.setpos(0,0)
+viewp.addstr("Viewport Position = (#{viewp.begx},#{viewp.begy})")
+viewp.refresh
+getch
+clear
+refresh
+
+viewp.move(1,1)
 viewp.setpos(0,0)
 viewp.addstr("Viewport Position = (#{viewp.begx},#{viewp.begy})")
 viewp.refresh
