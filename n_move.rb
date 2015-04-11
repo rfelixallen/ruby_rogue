f03d86cfc10dcfb13bacb5eef0481c4c00af376f
Robert Allen <rfelixallen@gmail.com>
2015-04-11 18:00:40 -0400

    converted most of the code, and it works!

--- n_move.rb
+++ n_move.rb
@@ -18,45 +18,44 @@ class Character
 		@py = py
 	end
 end
-=begin
-def mvwprintw(window, x, y, symb)
-	window.move(x,y)
-	window.addch("#{symb}")
-end
-=end
+
 def borders(window)
 	#window.clear
 	i = 0
-	while i <= (lines - 1) do
-		mvwprintw(window, i, 0, "|")
-		mvwprintw(window, i, cols - 1, "|")
+	w_y = []
+	w_x = []
+	Ncurses.getmaxyx(stdscr,w_y,w_x)
+	while i <= (w_y[0] - 1) do
+		mvwaddstr(window, i, 0, "|")
+		mvwaddstr(window, i, w_x[0] - 1, "|")
 		i += 1
 	end
 
 	j = 0
-	while j <= (cols - 1) do
-		mvwprintw(window, 0, j, "+")
-		mvwprintw(window, lines - 1, j, "+")
+	while j <= (w_x[0] - 1) do
+		mvwaddstr(window, 0, j, "+")
+		mvwaddstr(window, w_y[0] - 1, j, "+")
 		j += 1
 	end
-	window.refresh
+	Ncurses.refresh(window)
 end
 
 def simple_generate(window)
 	i = 1
-	max_x = window.maxx
-	max_y = window.maxy
-	while i < max_y - 1
+	w_y = []
+	w_x = []
+	Ncurses.getmaxyx(stdscr,w_y,w_x)
+	while i < w_x[0] - 1
 		j = 1
-		while j < max_x - 1
-			mvwprintw(window, i, j, "~")
-			#window.refresh
+		while j < w_y[0] - 1
+			mvwaddstr(window, i, j, "~")
+			#Ncurses.refresh(window)
 			j += 1
 		end
 		i += 1
 	end
 end
-
+=begin
 def center(subwin,parent,px,py)
 	rr = subwin.begx 	#Frame Positions
 	cc = subwin.begy 	#Frame Positions
@@ -94,32 +93,32 @@ def center(subwin,parent,px,py)
 
 	subwin.move(rr,cc) # mvderwin is the correct method
 end
-
+=end
 #################################################################################
 # Initialize 																 	#
 #################################################################################
 
-Ncurses.init_screen
+Ncurses.initscr
 Ncurses.noecho
 Ncurses.curs_set(0)
+Ncurses.cbreak
 Ncurses.stdscr # initialize stdscr? Might be active by default
+Ncurses.keypad(stdscr,true)
 sd_y = []
 sd_x = []
 Ncurses.getmaxyx(stdscr,sd_y,sd_x)
 
 # Welcome Streen
-Ncurses.move(sd_y[0] / 2, sd_x[0]  / 2)
-Ncurses.addstr("Welcome to Move!")
-Ncurses.move((sd_y[0] / 2) + 1, sd_x[0]  / 2)
-Ncurses.addstr("rows = #{sd_y[0]}, rows = #{sd_x[0]}")
+Ncurses.mvwaddstr(stdscr, sd_y[0] / 2, sd_x[0]  / 2, "Welcome to Move!")
+Ncurses.mvwaddstr(stdscr, sd_y[0] / 2 + 1, sd_x[0] / 2, "rows = #{sd_y[0]}, rows = #{sd_x[0]}")
 Ncurses.refresh
 Ncurses.getch
 Ncurses.clear
 Ncurses.refresh
 
 # Make Game Map
-field = Ncurses.newwin(sd_y * 2, sd_x * 2, 0, 0)
-viewp = Ncurses.subwin(field,sd_y, sd_x, 0, 0)
+field = Ncurses.newwin(sd_y[0] * 2, sd_x[0] * 2, 0, 0)
+viewp = Ncurses.subwin(field,sd_y[0], sd_x[0], 0, 0)
 
 # Draw borders, terrain and player
 #borders(viewp)
@@ -127,9 +126,9 @@ viewp = Ncurses.subwin(field,sd_y, sd_x, 0, 0)
 f_x = []
 f_y = []
 Ncurses.getmaxyx(field,f_y,f_x)
-p = Character.new((fy / 4), (fx / 4))
-Ncurses.mvwprintw(field, p.px, p.py, "#{p.symb}")
-Ncurses.refresh(viewp)
+p = Character.new((f_y[0] / 4), (f_x[0] / 4))
+Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+Ncurses.wrefresh(viewp)
 
 #################################################################################
 # Game Loop 																 	#
@@ -138,66 +137,63 @@ Ncurses.refresh(viewp)
 while 1
 =begin	
 	# Resize window to the terminal screen
-	new_y = stdscr.maxy
-	new_x = stdscr.maxx
+	new_y = []
+	new_x = []
+	Ncurses.getmaxyx(stdscr,new_y,new_x)
 
 	if (new_y != parent_y || new_x != parent_x)
 		Ncurses.wclear(viewp)
 
-		parent_x = new_x
-		parent_y = new_y
+		sd_x[0] = new_x[0]
+		sd_y[0] = new_y[0]
 
-		viewp.resize(new_y, new_x) # Resizes window to terminal screen
+		######viewp.resize(new_y, new_x) # Resizes window to terminal screen
 		borders(viewp) # Redraw new borders
 		simple_generate(field) # Put snow back on map
-		Ncurses.refresh(viewp)
-		mvwprintw(field, p.px, p.py, "#{p.symb}")
-		Ncurses.refresh(field)
+		Ncurses.wrefresh(viewp)
+		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+		Ncurses.wrefresh(field)
 	end
 =end
-	Ncurses.wmove(viewp,1,1)
-	viewp.addstr("Screen lines = #{parent_x}, Screen cols = #{parent_y}")
-	Ncurses.wmove(viewp,2,1)
-	viewp.addstr("Player lines = #{p.px}, Player cols = #{p.py}")
-	Ncurses.wmove(viewp,3,1)
-	viewp.addstr("Viewport Position = (#{viewp.begx},#{viewp.begy})")
-	Ncurses.refresh(viewp)
-	input = getch
+	Ncurses.mvwaddstr(viewp,1,1,"Screen lines = #{sd_y[0]}, Screen cols = #{sd_x[0]}")
+	Ncurses.mvwaddstr(viewp,2,1,"Player lines = #{p.px}, Player cols = #{p.py}")
+	Ncurses.wrefresh(viewp)
+	input = Ncurses.getch
 	case input
-    when 'w' # move up
+    when KEY_UP # move up
     	p.px -= 1 if p.px > 1
-	    	mvwprintw(field, p.px + 1, p.py, "\"") # Looks like footprints
-    		mvwprintw(field, p.px, p.py, "#{p.symb}")
-	    center(viewp,field,p.px,p.py)
-    	Ncurses.refresh(viewp)
-    when 's' # move down
-    	p.px += 1 if p.px < (field.maxy - 2)
-	    	mvwprintw(field, p.px - 1, p.py, "\"") # Looks like footprints
-    		mvwprintw(field, p.px, p.py, "#{p.symb}")
-	    center(viewp,field,p.px,p.py)
-    	Ncurses.refresh(viewp)
-    when 'd' # move right
-    	p.py += 1 if p.py < (field.maxx - 2)
-	    	mvwprintw(field, p.px, p.py - 1, "\"") # Looks like footprints
-    		mvwprintw(field, p.px, p.py, "#{p.symb}")
-	    center(viewp,field,p.px,p.py)
-    	Ncurses.refresh(viewp)
-	when 'a' # move left
+	    	Ncurses.mvwaddstr(field, p.px + 1, p.py, "\"") # Looks like footprints
+    		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+	    #center(viewp,field,p.px,p.py)
+    	Ncurses.wrefresh(viewp)
+    when KEY_DOWN # move down
+    	p.px += 1 if p.px < (f_y[0] - 2)
+	    	Ncurses.mvwaddstr(field, p.px - 1, p.py, "\"") # Looks like footprints
+    		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+	    #center(viewp,field,p.px,p.py)
+    	Ncurses.wrefresh(viewp)
+    when KEY_RIGHT # move right
+    	p.py += 1 if p.py < (f_x[0] - 2)
+	    	Ncurses.mvwaddstr(field, p.px, p.py - 1, "\"") # Looks like footprints
+    		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+	    #center(viewp,field,p.px,p.py)
+    	Ncurses.wrefresh(viewp)
+	when KEY_LEFT # move left
     	p.py -= 1 if p.py > 1
-	    	mvwprintw(field, p.px, p.py + 1, "\"") # Looks like footprints
-    		mvwprintw(field, p.px, p.py, "#{p.symb}")
-	    center(viewp,field,p.px,p.py)
-    	Ncurses.refresh(viewp)
-    when 'q' # Quit Game
+	    	Ncurses.mvwaddstr(field, p.px, p.py + 1, "\"") # Looks like footprints
+    		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
+	    #center(viewp,field,p.px,p.py)
+    	Ncurses.wrefresh(viewp)
+    when KEY_F2 # Quit Game
     	break
     else
     	Ncurses.flash
-    	Ncurses.refresh(viewp)
+    	Ncurses.wrefresh(viewp)
     end
 end
 
-Ncurses.wclear(field)
-Ncurses.mvwaddstr(field, lines / 2, cols / 2, "Good Bye!")
-Ncurses.refresh(field)
+Ncurses.clear
+Ncurses.mvwaddstr(stdscr, sd_y[0] / 2, sd_x[0] / 2, "Good Bye!")
+Ncurses.wrefresh(stdscr)
 Ncurses.getch
 Ncurses.endwin
