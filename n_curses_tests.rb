d831ebcc3c94861c0b70ddb2103de2567db6c278
Robert Allen <rfelixallen@gmail.com>
2015-04-11 10:11:41 -0400

    Finished converting the program! It works much better

--- n_curses_tests.rb
+++ n_curses_tests.rb
@@ -37,17 +37,17 @@ Ncurses.move(0,0)
 Ncurses.addstr("*")
 Ncurses.refresh
 
-Ncurses.napms(5000)
+Ncurses.napms(1000)
 Ncurses.move(0,(x[0] - 1))
 Ncurses.addstr("*")
 Ncurses.refresh
 
-Ncurses.napms(5000)
+Ncurses.napms(1000)
 Ncurses.move((y[0] - 1),0)
 Ncurses.addstr("*")
 Ncurses.refresh
 
-Ncurses.napms(5000)
+Ncurses.napms(1000)
 Ncurses.move((y[0] - 1),(x[0] - 1))
 Ncurses.addstr("*")
 Ncurses.refresh
@@ -65,75 +65,72 @@ Ncurses.refresh
 Ncurses.getch
 Ncurses.clear
 
-=begin
-
 # Set North Border
-move(0,0)
+Ncurses.move(0,0)
 i = 0
-while i < cols do
-	addch('-')
+while i < x[0] do
+	Ncurses.addstr('-')
 	i += 1
-	move(0,i)
+	Ncurses.move(0,i)
 end
-refresh
+Ncurses.refresh
 
 # Set West Border
-move(1,0)
+Ncurses.move(1,0)
 i = 0
-while i < lines do
-	addch('|')
+while i < y[0] do
+	Ncurses.addstr('|')
 	i += 1
-	move(i,0)
+	Ncurses.move(i,0)
 end
-refresh
+Ncurses.refresh
 
 # Set East Border
-move(1,(cols - 1))
+Ncurses.move(1,(x[0] - 1))
 i = 0
-while i < lines do
-	addch('|')
+while i < y[0] do
+	Ncurses.addstr('|')
 	i += 1
-	move(i,(cols-1))
+	Ncurses.move(i,(x[0]-1))
 end
-refresh
+Ncurses.refresh
 
 # Set South Border
-move((lines - 1),0)
+Ncurses.move((y[0] - 1),0)
 i = 0
-while i < cols do
-	addch('-')
+while i < x[0] do
+	Ncurses.addstr('-')
 	i += 1
-	move((lines - 1),i)
+	Ncurses.move((y[0] - 1),i)
 end
-refresh
+Ncurses.refresh
 
 # New test puts a message at the bottom of the screen
 # Set bottom message Top Border
-move((lines - 3),0)
+Ncurses.move((y[0] - 3),0)
 i = 0
-while i < cols do
-	addch('-')
+while i < x[0] do
+	Ncurses.addstr('-')
 	i += 1
-	move((lines - 3),i)
+	Ncurses.move((y[0] - 3),i)
 end
-refresh
+Ncurses.refresh
 
 # Set Message
-move(lines - 2, (cols - 10) / 2)
-addstr("***BOTTOM MESSAGE***")
-refresh
+Ncurses.move(y[0] - 2, (x[0] - 10) / 2)
+Ncurses.addstr("***BOTTOM MESSAGE***")
+Ncurses.refresh
 
-move((lines - 5) / 2, (cols - 10) / 2)
-addstr("Border Test Complete")
-refresh
-getch
-clear
+Ncurses.move((y[0] - 5) / 2, (x[0] - 10) / 2)
+Ncurses.addstr("Border Test Complete")
+Ncurses.refresh
+Ncurses.getch
 
 # Clear screen and give a goodbye message.
-clear
-move((lines - 5) / 2, (cols - 10) / 2)
-addstr("Goodbye!")
-refresh
-getch
-close_screen
-=end
+Ncurses.clear
+Ncurses.move((y[0] - 5) / 2, (x[0] - 10) / 2)
+Ncurses.addstr("Goodbye!")
+Ncurses.refresh
+Ncurses.getch
+Ncurses.clear # Removes characters, otherwise they're left on screen.
+Ncurses.endwin # Turns of colors, releases memory
