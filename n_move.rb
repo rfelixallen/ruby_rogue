f5c561feeb6361f3d5385e086e139504992e43c4
Robert Allen <rfelixallen@gmail.com>
2015-04-21 07:38:19 -0400

    added a console to print messages to

--- n_move.rb
+++ n_move.rb
@@ -43,6 +43,7 @@ def move_monster(window, orow, ocol, nrow, ncol, symb)
 	end
 end
 =end
+
 def terrain_tiles
 	# Pairs terrain name with icon
 	# Maybe this should be a class?
@@ -67,7 +68,7 @@ def move_character(window,p)
 end
 
 def borders(window)
-		# Draws borders and fills all game map tiles with snow.
+	# Draws borders and fills all game map tiles with snow.
 	i = 1
 	w_y = []
 	w_x = []
@@ -217,6 +218,7 @@ Ncurses.refresh
 #viewp = Ncurses.derwin(field,sd_y[0], sd_x[0], 0, 0)
 field = Ncurses.newwin(100, 100, 0, 0)
 viewp = Ncurses.derwin(field,25, 25, 0, 0) # Must not exceed size of terminal
+console = Ncurses.newwin(3, 25, 25, 0) # Must not exceed size of terminal
 
 # Draw borders, terrain and player
 #draw_map(field) # Draws a plain map with one terrain type.
@@ -238,6 +240,9 @@ end
 center(viewp,field,p.px,p.py)
 Ncurses.wrefresh(viewp)
 
+borders(console)
+Ncurses.mvwaddstr(console, 1, 2, "Hello!")
+Ncurses.wrefresh(console)
 #################################################################################
 # Game Loop 																 	#
 #################################################################################
@@ -269,7 +274,8 @@ while 1
 	case input
     when KEY_UP # move up
     	#p.px -= 1 if p.px > 1 && walkable.include?(Ncurses.mvwinch(field,p.px - 1, p.py)) 
-    	p.px -= 1 if walkable.include?(Ncurses.mvwinch(field,p.px - 1, p.py)) 
+    	step = Ncurses.mvwinch(field,p.px - 1, p.py)
+    	p.px -= 1 if step == " "
 			Ncurses.mvwaddstr(field, p.px + 1, p.py, " ") # for moving north
     		Ncurses.mvwaddstr(field, p.px, p.py, "#{p.symb}")
 			#move_character(field,p)
