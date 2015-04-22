f4aaaf549544d2ae9359b8452e9d0197d4c74b93
Robert Allen <rfelixallen@gmail.com>
2015-04-21 22:00:54 -0400

    starting a new perlin tutorial program

---
+++ perlin2.rb
@@ -0,0 +1,13 @@
+def GenerateWhiteNoise(width, height)
+	random = Random.new(0).rand # Random number using 0 as seed
+	noise = []
+
+	i = 0
+	while i < width
+		j = 0
+		while j < height
+			noise
+		end
+		i += 1
+	end
+end
