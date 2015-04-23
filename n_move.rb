b1ab37fde7307257c1f5924927ed511b672c343e
Robert Allen <rfelixallen@gmail.com>
2015-04-23 19:27:39 -0400

    took a crack at implementing a converted cpp implimentation of perlin_noise

--- n_move.rb
+++ n_move.rb
@@ -1,10 +1,11 @@
+require 'perlin_noise'
 require 'ncurses'
 include Ncurses
 
 ##################################################################################
-# TODO																			 #							 #
-#  	*Add Enemy																	 #
-#	*Add impassible terrain										                 #
+# TODO																			 #
+#   *Add Perlin Noise
+#  	*Add Enemy code																 #
 # 	*Add z-levels																 #
 #   *Add color																	 #
 ##################################################################################
@@ -146,8 +147,81 @@ def generate_random(window)
 	end
 end
 
+def fade(t)
+	return t * t * t * (t * (t * 6 - 15) + 10)
+end
+
+def lerp(t,a,b)
+	return a + t * (b - a)
+end
+
+def grad(hash,x,y,z)
+	h = hash & 15
+	u = h < 8 ? x : y
+	v = h < 4 ? y : h == 12 || h == 14 ? x : z
+	return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v)
+end
+
+def noise(x,y,z)
+	p = [151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208, 89,18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180]
+
+	# Find the unit cube that contains the point
+	X = x.floor & 255
+	Y = y.floor & 255
+	Z = z.floor & 255
+
+	# Find the relative x, y, z of point in cube
+	x -= x.floor
+	y -= y.floor
+	z -= z.floor
+
+	# Compute fade curves for x, y, z
+	u = x.fade
+	v = y.fade
+	w = z.fade
+
+	# Hash coordinates of the 8 cube corners
+	A = p[X] + Y
+	AA = p[A] + Z
+	AB = p[A + 1] + Z
+	B = p[X + 1] + Y
+	BA = p[B] + Z
+	BB = p[B + 1] + Z
+
+	res = lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), grad(p[BA], x-1, y, z)), lerp(u, grad(p[AB], x, y-1, z), grad(p[BB], x-1, y-1, z))),	lerp(v, lerp(u, grad(p[AA+1], x, y, z-1), grad(p[BA+1], x-1, y, z-1)), lerp(u, grad(p[AB+1], x, y-1, z-1),	grad(p[BB+1], x-1, y-1, z-1))))
+	return (res + 1.0)/2.0
+end
+
 def generate_perlin(window)
 	# Use Perlin Noise algorithim to draw terrain
+	borders(window)
+	
+	pn = Perlin::Noise.new 2
+
+	i = 1
+	w_y = []
+	w_x = []
+	Ncurses.getmaxyx(window,w_y,w_x)
+	while i < w_x[0] - 1
+		j = 1
+		while j < w_y[0] - 1
+			x = j / w_y[0]
+			y = i / w_x[0]
+			n = pn.noise(10 * x, 10 * y, 0.8)
+			case 
+			when n < 0.35
+				Ncurses.mvwaddstr(window, i, j, "#")
+			when n >= 0.35 && n < 0.6
+				Ncurses.mvwaddstr(window, i, j, "*")
+			when n >= 0.6 && n < 0.8
+				Ncurses.mvwaddstr(window, i, j, "^")
+			else
+				Ncurses.mvwaddstr(window, i, j, "~")
+			end
+		j += 1
+		end
+		i += 1
+	end
 end
 
 def center(subwin,parent,p_rows,p_cols)
@@ -230,8 +304,9 @@ console = Ncurses.newwin(3, 25, 25, 0) # Must not exceed size of terminal
 # Draw borders, terrain and player
 #draw_map(field) # Draws a plain map with one terrain type.
 walkable = ["32","126",32,126," ","~"] #walkable.include?('~')
-generate_random(field) # Draws a map with x random characters, randomly chosen for each pixel.
-building(field,10,10)
+#generate_random(field) # Draws a map with x random characters, randomly chosen for each pixel.
+generate_perlin(field)
+#building(field,10,10)
 f_x = []
 f_y = []
 Ncurses.getmaxyx(field,f_y,f_x)
