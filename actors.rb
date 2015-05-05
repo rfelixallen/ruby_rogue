4ec4e15864c40ddf8087c6b0772b495215f3b0b2
Robert Allen <rfelixallen@gmail.com>
2015-05-05 18:50:48 -0400

    moved character, monster class to actors, and divided other code between ui and terrain

--- actors.rb
+++ actors.rb
@@ -1,3 +1,23 @@
 def test_actors
 	puts "Actors Loaded!"
 end
+
+class Character
+  attr_accessor :symb, :px, :py, :hp
+  def initialize(px, py)
+    @symb = '@'
+    @px = px    # Player X location
+    @py = py    # Player Y location
+    @hp = 10    # Default Hit Points
+  end
+end
+
+class Monster
+  attr_accessor :symb, :mx, :my, :hp
+  def initialize(mx, my)
+    @symb = 'M'
+    @mx = mx 
+    @my = my
+    @hp = 3
+  end
+end
