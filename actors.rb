399d43dae6101a307ca72b0aa04406de68910d6b
Robert Allen <rfelixallen@gmail.com>
2015-05-05 23:39:08 -0400

    removed monster class

--- actors.rb
+++ actors.rb
@@ -1,17 +1,7 @@
 def test_actors
 	puts "Actors Loaded!"
 end
-=begin
-class Character
-  attr_accessor :symb, :xlines, :ycols, :hp
-  def initialize(symb, xlines, ycols)
-    @symb = symb
-    @xlines = xlines    # Player X location
-    @ycols = ycols    # Player Y location
-    @hp = 10    # Default Hit Points
-  end
-end
-=end
+
 class Character
   attr_accessor :symb, :xlines, :ycols, :hp
   def initialize(options = {})
@@ -19,15 +9,5 @@ class Character
     self.xlines = options[:xlines] || 2
     self.ycols = options[:ycols] || 2
     self.hp = options[:hp] || 3
-  end
-end
-
-class Monster
-  attr_accessor :symb, :mx, :my, :hp
-  def initialize(mx, my)
-    @symb = 'M'
-    @mx = mx 
-    @my = my
-    @hp = 3
   end
 end
