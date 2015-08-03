7f59fc04381600158dcd60cbbd5ab520d72160b9
Robert Allen <rfelixallen@gmail.com>
2015-08-03 19:32:01 -0400

    fixed script to choose between 3 tiles

--- testing/perlin.rb
+++ testing/perlin.rb
@@ -4,11 +4,12 @@ noise = Perlin::Noise.new 1, :interval => 200 # 1 is the dimension, interval is 
 
 0.step(300, 0.1).each do |x|
 	n = rand(0..100)
-	if n == 1..20
+	if n < 20
 		puts '#' * (noise[x] * 60).floor
-	elsif n == 21..60 
+	elsif n > 20 and n < 60 
 		puts '~' * (noise[x] * 60).floor
 	else
 		puts '@' * (noise[x] * 60).floor
 	end
+	#puts n
 end
